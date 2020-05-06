f3b91d60d6db2fd93e1921f9ee947571b7234c65
coltynw <coltynw@users.github.com>
2020-05-06 19:06:12 -0400

    fix for number 08 line 39 answer added

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -36,6 +36,6 @@ print(y)
 x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
-y = [x]
+y = [element for element in x if int(element) % 2 == 0]
 
 print(y)
